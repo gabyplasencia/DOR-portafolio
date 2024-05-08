688d89120c9bf7815961f76d523d60710b2c6972
Gabriela <gabyplasencia1995@gmail.com>
2024-05-08 17:04:26 +0100

    haciendo responsive la barra de navegación

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const HeaderNav = () => {
@@ -7,31 +7,42 @@ const HeaderNav = () => {
       pt: {name: "Português"},
       es: {name: "Español"},
     }
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const onChangeLang = (e) => {
-      const lang_code = e.target.value;
-      i18n.changeLanguage(lang_code);
-      };
+        const lang_code = e.target.value;
+        i18n.changeLanguage(lang_code);
+    };
+
+    const handleToggleClick = () => {
+      setMenuOpen(prevMenuOpen => !prevMenuOpen);
+    };
+
     return (
-    <header className="headerNav flex items-center w-vw md:justify-between md:px-9 md:py-5">
-        <img src="/assets/icons/logo-gp.svg" alt={t("Logo")} aria-hidden="true" className="logo invisible md:visible md:h-11"/>
-        <nav role="navigation">
-            <ul className="nav flex items-center md:gap-10 md:pb-5">
-                <li className="nav__item">
-                <a href="#hero" aria-label={t("ariaHomeLink")}>{t("inicio")}</a>
-                </li>
-                <li className="nav__item">
-                    <a href="#proyectos" aria-label={t("ariaProyectosLink")}>{t("proyectos")}</a>
-                </li>
-                <li className="nav__item">
-                    <a href="#contactame" aria-label={t("ariaFormularioLink")}>{t("contactame")}</a>
-                </li>
-            </ul>
-        </nav>
-        <select className="lan-select" defaultValue={i18n.language} onChange={onChangeLang}>{
-            Object.keys(lngs).map((lng) => (
-                <option type="submit" key={lng} value={lng}>{lngs[lng].name}</option>
-            ))}
-        </select>
-    </header>
-    )}
-export default HeaderNav
+        <header className="headerNav flex flex-wrap items-center w-vw p-4 md:justify-between md:px-9 md:py-2">
+            <img src="/assets/icons/logo-gp.svg" alt={t("Logo")} aria-hidden="true" className="logo hidden md:block md:h-11"/>
+            <button className={`nav-toggle block md:hidden ${menuOpen ? 'menu-open' : 'menu-close'}`} onClick={handleToggleClick}></button>
+            <nav role="navigation" className={`main-navbar  ${menuOpen ? 'menu-open' : 'menu-close'}`}>
+                <ul className="nav flex flex-col items-end pb-3 md:items-center md:gap-10 md:pb-1 md:flex-row">
+                    <li className="nav__item">
+                    <a href="#hero" aria-label={t("ariaHomeLink")}>{t("inicio")}</a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#proyectos" aria-label={t("ariaProyectosLink")}>{t("proyectos")}</a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#contactame" aria-label={t("ariaFormularioLink")}>{t("contactame")}</a>
+                    </li>
+                </ul>
+            </nav>
+            <select className="lan-select" defaultValue={i18n.language} onChange={onChangeLang}>
+                {Object.keys(lngs).map((lng) => (
+                    <option key={lng} value={lng}>{lngs[lng].name}</option>
+                ))}
+            </select>
+        </header>
+    );
+};
+
+export default HeaderNav;
+
