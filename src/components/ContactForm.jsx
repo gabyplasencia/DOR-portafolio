5bbb1f01b565704cc5eb79af92c5c744572f3c84
Gabriela <gabyplasencia1995@gmail.com>
2024-05-09 12:45:18 +0100

    estilos del formulario de contacto

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -24,11 +24,11 @@ const ContactForm = () => {
     }
 
     return (
-        <section className="contactame" id="contactame" >
-            <h2 className="section__title">{t("contactame")}</h2>
-            <form className="contact__form" method="/" onSubmit={send}>
+        <section className="contactame px-5 py-4 md:px-12 md:py-5 mx-auto text-center" id="contactame" >
+            <h2 className="contactame__title text-2xl font-bold">{t("contactame")}</h2>
+            <form className="contactame__form flex flex-col text-left my-3 md:my-5" method="/" onSubmit={send}>
                 <label htmlFor="name">{t("nombre")}</label>
-                <input type="text" id="name" name="name" value={name} placeholder={t("placeholderNombre")} required onChange={nameHandler}/>
+                <input type="text" id="name" name="name" value={name} placeholder="María Pérez" required onChange={nameHandler}/>
 
                 <label htmlFor="email">Email</label>
                 <input type="email" id="email" name="email" value={email} placeholder="maria@email.com" required onChange={emailHandler}/>
@@ -36,7 +36,7 @@ const ContactForm = () => {
                 <label htmlFor="message">{t("mensaje")}</label>
                 <textarea id="message" name="message" value={message} placeholder={t("placeholderMensaje")} required onChange={messageHandler}></textarea>
 
-                <button type="submit" className="contact__btn">{t("enviar")}</button>
+                <button type="submit" className="contactame__btn self-center px-8 py-2 mt-3 md:mt-6">{t("enviar")}</button>
             </form>
         </section>
     )
